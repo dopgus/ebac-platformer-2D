9cd8977f94a5170e6d4a844b6eef7710cddb690d
dopgus <gustavolimadacruz4@gmail.com>
2023-04-19 11:20:53 -0300

    finishing the effects

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -120,7 +120,8 @@ public class Player : MonoBehaviour
 
     private void PlayJumpVFX()
     {
-        if (jumpVFX != null) jumpVFX.Play();
+        VFXManager.Instance.PlayVFXByType(VFXManager.VFXType.JUMP, transform.position);
+        //if (jumpVFX != null) jumpVFX.Play();
     }
 
     /*private void HandleScaleJump()
